d0f0f582971b458858802d8a3cb240f40c2c50dd
Sebastián Mestre <36825825+SebastianMestre@users.noreply.github.com>
2020-04-16 21:50:59 -0300

    Parser: Add basic support for block statements
    - reorder member function definitions
    - add block ast node
    - add parser support for empty blocks

--- src/parser.cpp
+++ src/parser.cpp
@@ -19,6 +19,31 @@ bool handle_error (Writer<T>& lhs, Writer<U>&& rhs) {
 	return false;
 }
 
+
+Writer<Token const*> Parser::require (token_type t){
+	Token const* current_token = &m_lexer->current_token();
+
+	if(current_token->m_type != t){
+
+		std::stringstream ss;
+		ss
+			<< "Parse Error: @ "
+			<< current_token->m_line0+1 << ":" << current_token->m_col0+1
+			<< ": Expected "
+			<< token_type_string[int(t)]
+			<< " but got "
+			<< token_type_string[int(current_token->m_type)]
+			<< " instead";
+
+		return {ParseError{ss.str()}};
+	}
+
+	m_lexer->advance();
+
+	return make_writer(current_token);
+}
+
+
 Writer<std::unique_ptr<AST>> Parser::parse_top_level () {
 	Writer<std::unique_ptr<AST>>  result = {
 		{"Parse Error: Failed to parse top level program"}
@@ -129,37 +154,31 @@ Writer<std::unique_ptr<AST>> Parser::parse_function () {
 	if(handle_error(result, require(token_type::PAREN_CLOSE)))
 		return result;
 
-
-	if(handle_error(result, require(token_type::BRACE_OPEN)))
+	auto block = parse_block();
+	if(handle_error(result, block)){
 		return result;
-	// TODO: parse function body
-	if(handle_error(result, require(token_type::BRACE_CLOSE)))
-		return result;
-
+	}
 
 	auto e = std::make_unique<ASTFunction>();
+	e->m_body = std::move(block.m_result);
+
 	return make_writer<std::unique_ptr<AST>>(std::move(e));
 }
 
-Writer<Token const*> Parser::require (token_type t){
-	Token const* current_token = &m_lexer->current_token();
+Writer<std::unique_ptr<AST>> Parser::parse_block () {
+	Writer<std::unique_ptr<AST>>  result = {
+		{"Parse Error: Failed to parse block statement"}
+	};
 
-	if(current_token->m_type != t){
-
-		std::stringstream ss;
-		ss
-			<< "Parse Error: @ "
-			<< current_token->m_line0+1 << ":" << current_token->m_col0+1
-			<< ": Expected "
-			<< token_type_string[int(t)]
-			<< " but got "
-			<< token_type_string[int(current_token->m_type)]
-			<< " instead";
-
-		return {ParseError{ss.str()}};
+	if(handle_error(result, require(token_type::BRACE_OPEN))){
+		return result;
 	}
 
-	m_lexer->advance();
+	if(handle_error(result, require(token_type::BRACE_CLOSE))){
+		return result;
+	}
 
-	return make_writer(current_token);
+	auto e = std::make_unique<ASTBlock>();
+
+	return make_writer<std::unique_ptr<AST>>(std::move(e));
 }
