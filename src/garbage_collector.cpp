e95d7963f58ce1b1bc2062464f53620b0ebbe839
SebastianMestre <mestresebastianunr@gmail.com>
2020-04-18 15:04:01 -0300

    GC: Initial Implementation

--- src/garbage_collector.cpp
+++ src/garbage_collector.cpp
@@ -1,1 +1,45 @@
 #include "garbage_collector.hpp"
+
+#include <algorithm>
+
+namespace GarbageCollector {
+
+void GC::run() {
+	for (auto* block : m_blocks) {
+		block->visited = false;
+	}
+
+	for (auto* root : m_roots) {
+		root->gc_visit();
+	}
+
+	for (auto*& block : m_blocks) {
+		if (not block->visited) {
+			delete block;
+			block = nullptr;
+		}
+	}
+
+	auto is_null = [&](Type::Value* p) { return p == nullptr; };
+
+	m_blocks.erase(
+	    std::remove_if(m_blocks.begin(), m_blocks.end(), is_null), m_blocks.end());
+}
+
+void GC::add_root(Type::Value* new_root) {
+	m_roots.push_back(new_root);
+}
+
+Type::Object* GC::new_object() {
+	auto result = new Type::Object;
+	m_blocks.push_back(result);
+	return result;
+}
+
+Type::List* GC::new_list() {
+	auto result = new Type::List;
+	m_blocks.push_back(result);
+	return result;
+}
+
+} // namespace GarbageCollector
