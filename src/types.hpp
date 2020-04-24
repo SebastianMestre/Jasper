37261a0ab9ef375c4a0f5d5d8df018f62ee41aeb
Luciano Barletta <lushobarlett@gmail.com>
2020-04-24 10:27:39 -0300

    Types: add Scope::declare

--- src/types.hpp
+++ src/types.hpp
@@ -24,7 +24,7 @@ struct Scope {
 	Scope* m_parent;
 	ObjectType m_declarations;
 
-	Value* access(Identifier i) {
+	Value* access(const Identifier& i) {
 		auto v = m_declarations.find(i);
 		
 		if (v != m_declarations.end())
@@ -35,6 +35,11 @@ struct Scope {
 
 		// TODO: ReferenceError
 		return nullptr;
+	}
+
+	void declare(const Identifier& i, Value* v) {
+		// TODO: check name colission
+		m_declarations[i] = v;
 	}
 };
 
