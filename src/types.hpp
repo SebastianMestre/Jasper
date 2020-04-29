77466d56d91184cd723987afc94bc4fc30df68bb
Luciano Barletta <lushobarlett@gmail.com>
2020-04-29 16:37:28 -0300

    Runtime: add scope methods and environment short-hands

--- src/types.hpp
+++ src/types.hpp
@@ -24,6 +24,9 @@ struct Scope {
 	Scope* m_parent {nullptr};
 	ObjectType m_declarations;
 
+	Scope() = default;
+	Scope(Scope* parent) : m_parent(parent) {}
+
 	Value* access(const Identifier& i) {
 		auto v = m_declarations.find(i);
 		
@@ -46,6 +49,34 @@ struct Scope {
 struct Environment {
 	GarbageCollector::GC* m_gc;
 	Scope* m_scope;
+
+	Scope* new_scope() {
+		Scope* parent = m_scope;
+		m_scope = new Scope(parent);
+		return m_scope;
+	}
+
+	void end_scope() {
+		Scope* parent = m_scope->m_parent;
+		delete m_scope;
+		m_scope = parent;
+	}
+
+	// used as a short-hand
+	
+	// scope
+	inline void declare(const Identifier& i, Value* v) { m_scope->declare(i,v); }
+	inline Value* access(const Identifier& i) { return m_scope->access(i); }
+	
+	// gargabe_collector
+	inline Null* null () { return m_gc->null(); }
+	inline Integer* new_integer (int i) { return m_gc->new_integer(i); }
+	inline Float* new_float (float f) { return m_gc->new_float(f); }
+	inline String* new_string (std::string s) { return m_gc->new_string(s); }
+	inline List* new_list () { return m_gc->new_list(); }
+	inline Object* new_object () { return m_gc->new_object(); }
+	inline Function* new_function (FunctionType def, Scope* s) { return m_gc->new_function(def, s); }
+	inline Error* new_error (std::string e) { return m_gc->new_error(e); }
 };
 
 }
