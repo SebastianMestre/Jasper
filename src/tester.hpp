a703516ef90bbdc51eee35b25b1e17b7b8342f96
Luciano Barletta <37297979+lushoBarlett@users.noreply.github.com>
2020-05-09 13:35:18 -0300

    Separate tests (#40)
    * Main: extract function make_parser
    * Tester: new struct and files for tester
    * Main: move tests to Tester environment
    * Tests: move tests to new test_file, create namespace Test

---
+++ src/tester.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include "environment.hpp"
+#include "parser.hpp"
+
+namespace Test {
+
+using TestFunction = bool (*)(Type::Environment);
+
+Parser make_parser(std::string source, Lexer& l);
+
+struct Tester {
+
+	std::string m_source;
+	std::vector<TestFunction> m_testers;
+
+	Tester(std::string);
+	Tester(std::string, std::vector<TestFunction>);
+
+	void add_test(TestFunction);
+	bool execute(bool);
+};
+
+}
