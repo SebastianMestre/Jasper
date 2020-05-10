dcf60aaef5bad2f15ea8acb051068cd771fe383b
Sebastián Mestre <36825825+SebastianMestre@users.noreply.github.com>
2020-05-10 17:31:42 -0300

    Move tests (#43)
    * Testing: move all tests to tests file. Read main code from file.
    
    * Testing: make CircleCI run tests in a separate job from the build

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cassert>
 
+#include "eval.hpp"
 #include "tester.hpp"
 #include "value.hpp"
 
@@ -16,7 +17,7 @@ int main() {
 	)");
 
 	bexp_tester.add_test(
-		[](Type::Environment env) {
+		+[](Type::Environment& env) -> bool {
 			auto* expected_10 = env.m_scope->access("int_val");
 			auto* as_integer = dynamic_cast<Type::Integer*>(expected_10);
 			assert(as_integer);
@@ -51,5 +52,75 @@ int main() {
 		}
 	);
 
+	Tester monolithic_test = {R"(
+	x : dec = 1.4;
+	y : int = 3;
+	z := fn () {
+		a := 2;
+		b : dec = (4 + 5) * 3 * 1 + 4.5 * 2;
+		c := a;
+		a + b;
+		cmp : bool = a < c;
+
+		sqrt5 := obt{x:=1;y:=2;} |> norm();
+		sixteen := my_lib.times_pi(my_lib.five()) + 1;
+	};
+
+	y := fn () {
+		print(1,z(),4);
+	};
+
+	w := fn (a,b:int,c){};
+
+	f := obt {
+		greeting := "Hello, ";
+		__invoke := fn (name : string) {
+			print(greeting + name);
+		};
+	};
+
+	f := fn (name : string) {};
+
+	sqrt := fn (x) { return (x+1) * 0.5; };
+
+	norm := fn(p){
+		return sqrt(p.x * p.x + p.y * p.y);
+	};
+
+	my_lib := dict {
+		five := fn() { return 6; };
+		times_pi := fn(x) { return x * 3; };
+	};
+
+	__invoke := fn () {
+		f.greeting = "Hey, ";
+		f("Sailor");
+	};
+
+	names := dict {
+		user0 := "peter";
+		user1 := "joseph";
+		user2 := "anne";
+	};
+
+)"};
+
+	monolithic_test.add_test(+[](Type::Environment& env) -> bool {
+		// NOTE: We currently implement funcion evaluation in eval(ASTCallExpression)
+		// this means we need to create a call expression node to run the program.
+		// TODO: We need to clean this up
+		auto top_level_name = std::make_unique<ASTIdentifier>();
+		top_level_name->m_text = "__invoke";
+
+		auto top_level_call = std::make_unique<ASTCallExpression>();
+		top_level_call->m_callee = std::move(top_level_name);
+		top_level_call->m_args = std::make_unique<ASTArgumentList>();
+
+		eval(top_level_call.get(), env);
+
+		return true;
+	});
+
 	assert(bexp_tester.execute(true));
+	assert(monolithic_test.execute(true));
 }
