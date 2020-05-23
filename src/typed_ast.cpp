b06c5b70e4da53a958b2cc56f6097282722d693a
Gastonm123 <38297466+Gastonm123@users.noreply.github.com>
2020-05-23 15:45:38 -0300

    Add a runtime variant of the AST  (#70)
    This should help simplify the runtime, and separate concerns over different modules.
    
    Co-authored-by: Gaston <gastonmartinezcasto@gmail.com>
    Co-authored-by: SebastianMestre <mestresebastianunr@gmail.com>

---
+++ src/typed_ast.cpp
@@ -0,0 +1,222 @@
+#include <cassert>
+
+#include "typed_ast.hpp"
+#include "ast.hpp"
+#include "ast_type.hpp"
+
+std::unique_ptr<TypedAST> get_unique(std::unique_ptr<AST>& ast) {
+    return std::unique_ptr<TypedAST>(convertAST(ast.get()));
+}
+
+TypedAST* convertAST(ASTNumberLiteral* ast) {
+    auto typed_number = new TypedASTNumberLiteral;
+
+    // desambiguar el tipo en float
+    // por defecto es int
+    // chequeo si es float:
+    //      typed_number->m_vtype = value_type::Float
+
+    typed_number->m_token = ast->m_token;
+    return typed_number;
+}
+
+TypedAST* convertAST(ASTStringLiteral* ast) {
+    auto typed_string = new TypedASTStringLiteral;
+    typed_string->m_token = ast->m_token;
+    return typed_string;
+}
+
+TypedAST* convertAST(ASTBooleanLiteral* ast) {
+    auto typed_boolean = new TypedASTBooleanLiteral;
+    typed_boolean->m_token = ast->m_token;
+    return typed_boolean;
+}
+
+TypedAST* convertAST(ASTNullLiteral* ast) {
+    return new TypedASTNullLiteral;
+}
+
+TypedAST* convertAST(ASTObjectLiteral* ast) {
+    auto typed_object = new TypedASTObjectLiteral;
+    // al tipo de los objetos se les debe sumar una identificacion
+    // de clase
+
+    for (auto& element : ast->m_body) {
+        typed_object->m_body.push_back(get_unique(element));
+    }
+
+    return typed_object;
+}
+
+TypedAST* convertAST(ASTArrayLiteral* ast) {
+    auto typed_array = new TypedASTArrayLiteral;
+
+    for (auto& element : ast->m_elements) {
+        typed_array->m_elements.push_back(get_unique(element)); 
+    }
+
+    return typed_array;
+}
+
+TypedAST* convertAST(ASTDictionaryLiteral* ast) {
+    auto typed_dict = new TypedASTDictionaryLiteral;
+
+    for (auto& element : ast->m_body) {
+        typed_dict->m_body.push_back(get_unique(element));
+    }
+
+    return typed_dict;
+}
+
+TypedAST* convertAST(ASTFunctionLiteral* ast) {
+    auto typed_function = new TypedASTFunctionLiteral;
+
+    for (auto& arg : ast->m_args) {
+        typed_function->m_args.push_back(get_unique(arg));
+    }
+
+    for (auto& captr : ast->m_captures) {
+        typed_function->m_captures.push_back(captr);
+    }
+
+    typed_function->m_body = get_unique(ast->m_body);
+
+    return typed_function;
+}
+
+TypedAST* convertAST (ASTDeclarationList* ast) {
+    auto typed_declist = new TypedASTDeclarationList;
+
+    for (auto& declaration : ast->m_declarations) {
+        typed_declist->m_declarations.push_back(get_unique(declaration));
+    }
+
+    return typed_declist;
+}
+
+TypedAST* convertAST(ASTDeclaration* ast) {
+    auto typed_dec = new TypedASTDeclaration;
+
+    typed_dec->m_identifier_token = ast->m_identifier_token;
+    typed_dec->m_typename_token   = ast->m_typename_token;
+    typed_dec->m_value            = get_unique(ast->m_value);
+
+    return typed_dec;
+}
+
+TypedAST* convertAST(ASTIdentifier* ast) {
+    auto typed_id = new TypedASTIdentifier;
+    typed_id->m_token = ast->m_token;
+    return typed_id;
+}
+
+TypedAST* convertAST(ASTBinaryExpression* ast) {
+    auto typed_be = new TypedASTBinaryExpression;
+
+    typed_be->m_op  = ast->m_op;
+    typed_be->m_lhs = get_unique(ast->m_lhs);
+    typed_be->m_rhs = get_unique(ast->m_rhs);
+
+    return typed_be;
+}
+
+TypedAST* convertAST(ASTCallExpression* ast) {
+    auto typed_ce = new TypedASTCallExpression;
+
+    for (auto& arg : ast->m_args) {
+        typed_ce->m_args.push_back(get_unique(arg));
+    }
+
+    typed_ce->m_callee = get_unique(ast->m_callee);
+
+    return typed_ce;
+}
+
+TypedAST* convertAST(ASTIndexExpression* ast) {
+    auto typed_index = new TypedASTIndexExpression;
+
+    typed_index->m_callee = get_unique(ast->m_callee);
+    typed_index->m_index  = get_unique(ast->m_index);
+
+    return typed_index;
+}
+
+TypedAST* convertAST(ASTBlock* ast) {
+    auto typed_block = new TypedASTBlock;
+
+    for (auto& element : ast->m_body) {
+        typed_block->m_body.push_back(get_unique(element));
+    }
+
+    return typed_block;
+}
+
+TypedAST* convertAST(ASTReturnStatement* ast) {
+    auto typed_rs = new TypedASTReturnStatement;
+
+    typed_rs->m_value = get_unique(ast->m_value);
+
+    return typed_rs;
+}
+
+TypedAST* convertAST(ASTIfStatement* ast) {
+    auto typed_if = new TypedASTIfStatement;
+
+    typed_if->m_condition = get_unique(ast->m_condition);
+    typed_if->m_body      = get_unique(ast->m_body);
+
+    return typed_if;
+}
+
+TypedAST* convertAST(ASTForStatement* ast) {
+    auto typed_for = new TypedASTForStatement;
+
+    typed_for->m_declaration = get_unique(ast->m_declaration);
+    typed_for->m_condition   = get_unique(ast->m_condition);
+    typed_for->m_action      = get_unique(ast->m_action);
+    typed_for->m_body        = get_unique(ast->m_declaration);
+
+    return typed_for;
+}
+
+TypedAST* convertAST (AST* ast) {
+    switch (ast->type()) {
+    case ast_type::NumberLiteral:
+        return convertAST(static_cast<ASTNumberLiteral*>(ast));
+    case ast_type::StringLiteral:
+        return convertAST(static_cast<ASTStringLiteral*>(ast));
+    case ast_type::BooleanLiteral:
+        return convertAST(static_cast<ASTBooleanLiteral*>(ast));
+    case ast_type::NullLiteral:
+        return convertAST(static_cast<ASTNullLiteral*>(ast));
+    case ast_type::ObjectLiteral:
+        return convertAST(static_cast<ASTObjectLiteral*>(ast));
+    case ast_type::ArrayLiteral:
+        return convertAST(static_cast<ASTArrayLiteral*>(ast));
+    case ast_type::DictionaryLiteral:
+        return convertAST(static_cast<ASTDictionaryLiteral*>(ast));
+    case ast_type::FunctionLiteral:
+        return convertAST(static_cast<ASTFunctionLiteral*>(ast));
+    case ast_type::DeclarationList:
+        return convertAST(static_cast<ASTDeclarationList*>(ast));
+    case ast_type::Declaration:
+        return convertAST(static_cast<ASTDeclaration*>(ast));
+    case ast_type::Identifier:
+        return convertAST(static_cast<ASTIdentifier*>(ast));
+    case ast_type::BinaryExpression:
+        return convertAST(static_cast<ASTBinaryExpression*>(ast));
+    case ast_type::CallExpression:
+        return convertAST(static_cast<ASTCallExpression*>(ast));
+    case ast_type::IndexExpression:
+        return convertAST(static_cast<ASTIndexExpression*>(ast));
+    case ast_type::Block:
+        return convertAST(static_cast<ASTBlock*>(ast));
+    case ast_type::ReturnStatement:
+        return convertAST(static_cast<ASTReturnStatement*>(ast));
+    case ast_type::IfStatement:
+        return convertAST(static_cast<ASTIfStatement*>(ast));
+    case ast_type::ForStatement:
+        return convertAST(static_cast<ASTForStatement*>(ast));
+    }
+}
+
---
+++ src/typed_ast.hpp
@@ -0,0 +1,168 @@
+#pragma once
+
+#include "ast_type.hpp"
+#include "value_type.hpp"
+#include "ast.hpp"
+
+struct TypedAST {
+protected:
+	ast_type m_type;
+
+public:
+    value_type m_vtype;
+	TypedAST(ast_type type) : m_type{ type } {}
+    TypedAST(ast_type type, value_type vtype) : m_type {type}, m_vtype {vtype} {}
+
+	ast_type type() const { return m_type; }
+	virtual ~TypedAST() = default;
+};
+
+TypedAST* convertAST(AST*);
+std::unique_ptr<TypedAST> get_unique(std::unique_ptr<AST>&);
+
+// las estructuras como declaration list, index expression, block, if, for no tienen
+// tipo de valor asociado
+struct TypedASTNumberLiteral : public TypedAST {
+	Token const* m_token;
+
+	std::string const& text () { return m_token->m_text; }
+
+	TypedASTNumberLiteral() : 
+        TypedAST{ ast_type::NumberLiteral, value_type::Integer } {}
+};
+
+struct TypedASTStringLiteral : public TypedAST {
+	Token const* m_token;
+
+	std::string const& text () { return m_token->m_text; }
+
+	TypedASTStringLiteral() : 
+        TypedAST{ ast_type::StringLiteral, value_type::String } {}
+};
+
+struct TypedASTBooleanLiteral : public TypedAST {
+	Token const* m_token;
+
+	std::string const& text () { return m_token->m_text; }
+
+	TypedASTBooleanLiteral() : 
+        TypedAST{ ast_type::BooleanLiteral, value_type::Boolean } {}
+};
+
+struct TypedASTNullLiteral : public TypedAST {
+
+	TypedASTNullLiteral() : 
+        TypedAST{ ast_type::NullLiteral, value_type::Null } {}
+};
+
+struct TypedASTObjectLiteral : public TypedAST {
+	std::vector<std::unique_ptr<TypedAST>> m_body;
+
+    // future feature 
+    // the value type for objects must be followeb by a class identifier
+	TypedASTObjectLiteral() : 
+        TypedAST{ ast_type::ObjectLiteral, value_type::Object } {}
+};
+
+struct TypedASTArrayLiteral : public TypedAST {
+	std::vector<std::unique_ptr<TypedAST>> m_elements;
+
+	TypedASTArrayLiteral() : 
+        TypedAST{ ast_type::ArrayLiteral, value_type::Array } {}
+};
+
+struct TypedASTDictionaryLiteral : public TypedAST {
+	std::vector<std::unique_ptr<TypedAST>> m_body;
+
+	TypedASTDictionaryLiteral() : 
+        TypedAST{ ast_type::DictionaryLiteral, value_type::Dictionary } {}
+};
+
+struct TypedASTFunctionLiteral : public TypedAST {
+	std::unique_ptr<TypedAST> m_body;
+	std::vector<std::unique_ptr<TypedAST>> m_args;
+	std::vector<std::string> m_captures;
+
+	TypedASTFunctionLiteral() : 
+        TypedAST{ ast_type::FunctionLiteral } {}
+};
+
+// doesnt have a value_type
+struct TypedASTDeclarationList : public TypedAST {
+	std::vector<std::unique_ptr<TypedAST>> m_declarations;
+
+	TypedASTDeclarationList() : TypedAST{ ast_type::DeclarationList } {}
+};
+
+// doesnt have a value_type
+struct TypedASTDeclaration : public TypedAST {
+	Token const* m_identifier_token;
+	Token const* m_typename_token { nullptr };
+	std::unique_ptr<TypedAST> m_value;
+
+	std::string const& identifier_text() const { return m_identifier_token->m_text; }
+	std::string const& typename_text() const { return m_typename_token->m_text; }
+
+	TypedASTDeclaration() : TypedAST{ ast_type::Declaration } {}
+};
+
+// the value_type must be computed
+struct TypedASTIdentifier : public TypedAST {
+	Token const* m_token;
+
+	std::string const& text () { return m_token->m_text; }
+
+	TypedASTIdentifier() : TypedAST{ ast_type::Identifier } {}
+};
+
+// the value depends on the operator
+struct TypedASTBinaryExpression : public TypedAST {
+	token_type m_op;
+	std::unique_ptr<TypedAST> m_lhs;
+	std::unique_ptr<TypedAST> m_rhs;
+
+	TypedASTBinaryExpression() : TypedAST{ ast_type::BinaryExpression } {}
+};
+
+// the value depends on the return value of callee
+struct TypedASTCallExpression : public TypedAST {
+	std::unique_ptr<TypedAST> m_callee;
+	std::vector<std::unique_ptr<TypedAST>> m_args;
+
+	TypedASTCallExpression() : TypedAST{ ast_type::CallExpression } {}
+};
+
+struct TypedASTIndexExpression : public TypedAST {
+	std::unique_ptr<TypedAST> m_callee;
+	std::unique_ptr<TypedAST> m_index;
+
+	TypedASTIndexExpression() : TypedAST{ ast_type::IndexExpression } {}
+};
+
+struct TypedASTBlock : public TypedAST {
+	std::vector<std::unique_ptr<TypedAST>> m_body;
+
+	TypedASTBlock() : TypedAST{ ast_type::Block } {}
+};
+
+struct TypedASTReturnStatement : public TypedAST {
+	std::unique_ptr<TypedAST> m_value;
+
+	TypedASTReturnStatement() : TypedAST{ ast_type::ReturnStatement } {}
+};
+
+struct TypedASTIfStatement : public TypedAST {
+	std::unique_ptr<TypedAST> m_condition;
+	std::unique_ptr<TypedAST> m_body;
+
+	TypedASTIfStatement() : TypedAST{ ast_type::IfStatement } {}
+};
+
+struct TypedASTForStatement : public TypedAST {
+	std::unique_ptr<TypedAST> m_declaration;
+	std::unique_ptr<TypedAST> m_condition;
+	std::unique_ptr<TypedAST> m_action;
+	std::unique_ptr<TypedAST> m_body;
+
+	TypedASTForStatement() : TypedAST{ ast_type::ForStatement } {}
+};
