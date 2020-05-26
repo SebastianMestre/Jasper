c433928c9213635a10214dc5ab4ae8f5c29485ab
Gaston <gastonmartinezcasto@gmail.com>
2020-05-26 04:04:48 -0300

    Commit controversial. Not sure about the names

--- src/typed_ast_type.hpp
+++ src/typed_ast_type.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <vector>
+
+// runtime type
 enum class ast_vtype {
     Null,
 	
@@ -13,11 +16,23 @@ enum class ast_vtype {
 	Object,
 	Dictionary,
 	
-	Function,
-	NativeFunction,
-	
 	TypeError,
 	Void,
-	Undefined,
-	Wildcard
+	Undefined
+};
+
+class ValueType {
+private:
+	bool m_function;
+public:
+	ast_vtype m_vtype {ast_vtype::Undefined};
+	std::vector<ast_vtype> m_vargs;
+	int m_wildcard;
+
+	ValueType(ast_vtype type) : m_vtype{type} {}
+	ValueType(bool is_function) : m_function{is_function} {}
+
+	bool operator== (ast_vtype);
+	bool operator!= (ast_vtype);
+	bool operator!= (ValueType);
 };
