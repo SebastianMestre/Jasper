21b094735967f33d4e91a42cea9c2f993fa9ab5f
Gaston <gastonmartinezcasto@gmail.com>
2020-05-26 20:50:19 -0300

    Funcion createGraph y createDag

--- src/type_checker.cpp
+++ src/type_checker.cpp
@@ -4,15 +4,66 @@
 
 namespace TypeChecker {
 
-void TC::type(TypedAST* root) {
-    traverse(root);
-    m_root = root;
+void TC::createDag(TypedAST* root) {
+    createGraph(root);
+
+    // traverse graph
+    for (auto& node : m_graph) {
+        
+    }
+
+    // dfs on traversed graph
+    for (auto& node : m_graph) {
+        // visit(node);
+    }
 }
 
-void TC::traverse(TypedAST* node) {
+GraphNode* createGraph(TypedASTDeclaration* ast) {
+    auto node_representation = new GraphNode {ast};
+    auto value = createGraph(ast->m_value.get());
+    node_representation->m_next.push_back(value);
+    return node_representation;
+}
+
+GraphNode* createGraph(TypedASTDeclarationList* ast) {
+    auto node = new GraphNode {ast};
+    for (auto& decl : ast->m_declarations) {
+        auto decl_node = createGraph(decl.get());
+        node->m_next.push_back(decl_node);
+    }
+    return node;
+}
+
+GraphNode* createGraph(TypedASTBinaryExpression* ast) {
+    auto node = new GraphNode {ast};
+
+    auto lhs = createGraph(ast->m_lhs.get());
+    auto rhs = createGraph(ast->m_rhs.get());
+
+    node->m_next.push_back(lhs);
+    node->m_next.push_back(rhs);
+
+    return node;
+}
+
+GraphNode* createGraph(TypedASTFunctionLiteral* ast) {
+    auto node = new GraphNode {ast};
+
+    auto body = createGraph(ast->m_body.get());
+    node->m_next.push_back(body);
+
+    for (auto& arg : ast->m_args) {
+        auto arg_node = createGraph(arg.get());
+        node->m_next.push_back(arg_node);
+    }
+
+    return node;
+}
+
+GraphNode* createGraph(TypedAST* node) {
     switch(node->type()) {
     case ast_type::Declaration:
-        return 
+        return createGraph(static_cast<TypedASTDeclaration*>(node));
     }
 }
 
--- src/type_checker.hpp
+++ src/type_checker.hpp
@@ -6,17 +6,29 @@
 
 namespace TypeChecker {
 
-class TypeGraph {
+class GraphNode {
+public:
+    bool m_visited {false};
+    TypedAST* m_value;
+    std::vector<GraphNode*> m_next;
+
+    GraphNode(TypedAST* value) : m_value{value} {}
+};
+
+class GraphComponent {
+public:
+    std::vector<TypedAST*> m_body;
 };
 
 class TC {
 protected:
-    TypeGraph m_root;
+    GraphComponent m_root;
 public:
-    std::vector<TypeGraph> m_traverse;
-    void type(TypedAST*);
-    void traverse(TypedAST*);
+    std::vector<GraphNode*> m_graph;
+    std::vector<GraphComponent*> m_dag; 
+    void createDag(TypedAST*);
 };
 
+GraphNode* createGraph(TypedAST*);
 
 }
