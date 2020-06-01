86d259b75ec4acb645b0b76895a973ee7339230f
SebastianMestre <mestresebastianunr@gmail.com>
2020-06-01 17:25:54 -0300

    Add a type checker to run on top of the type system

---
+++ src/typechecker.cpp
@@ -0,0 +1,43 @@
+#include "typechecker.hpp"
+
+#include "typed_ast.hpp"
+
+#include <cassert>
+
+TypeChecker::TypeChecker() {
+	// arrow, for functions
+	m_core.type_function_data.push_back({ -1 });
+}
+
+MonoId TypeChecker::rule_var(PolyId poly) {
+	return m_core.inst_fresh(poly);
+}
+
+// Hindley-Milner [App], modified for multiple argument functions.
+MonoId TypeChecker::rule_app(std::vector<MonoId> args_types, MonoId func_type) {
+	MonoId return_type = m_core.new_var();
+
+	args_types.push_back(return_type);
+	MonoId deduced_func_type
+	    = m_core.new_term(m_core.arrow_type_function(), std::move(args_types));
+
+	m_core.unify(func_type, deduced_func_type);
+
+	return return_type;
+}
+
+// TODO
+MonoId TypeChecker::rule_abs() {
+	assert(0);
+}
+
+// TODO
+MonoId TypeChecker::rule_let(MonoId mono) {
+	assert(0);
+	// return generalize(mono);
+}
+
+// TODO
+MonoId TypeChecker::rule_rec() {
+	assert(0);
+}
---
+++ src/typechecker.hpp
@@ -0,0 +1,14 @@
+#include "typesystem.hpp"
+
+struct TypeChecker {
+
+	TypeSystemCore m_core;
+
+	TypeChecker();
+
+	MonoId rule_var(PolyId poly);
+	MonoId rule_app(std::vector<MonoId> args_types, MonoId func_type);
+	MonoId rule_abs();
+	MonoId rule_let(MonoId mono);
+	MonoId rule_rec();
+};
