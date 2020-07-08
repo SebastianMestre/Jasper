f00a6557f50e92d75bef3e59154dbb0b35d11142
Luciano Barletta <lushobarlett@gmail.com>
2020-07-08 17:52:03 -0300

    Tester: implement Tester::add_tests

--- src/tester.cpp
+++ src/tester.cpp
@@ -17,6 +17,11 @@ void Tester::add_test(TestFunction tf) {
 	m_testers.push_back(tf);
 }
 
+void Tester::add_tests(const std::vector<TestFunction>& tfs) {
+	for (auto* tf : tfs)
+		m_testers.push_back(tf);
+}
+
 int Tester::execute(bool print_parse) {
 	for (auto* tester : m_testers) {
 		int exit_code = ::execute(m_source, print_parse, tester);
