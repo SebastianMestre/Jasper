fb333e3868c28e7e157766a131340af4db921907
Luciano Barletta <lushobarlett@gmail.com>
2020-07-10 14:00:30 -0300

    Tests: refactor and implement array size test
    Implements Assert::array_of_size. Extracts a function Assert::of_type that takes a Type::Value and doesn't print `Success` at the end, it's a helper function. But if we change the fact that the test utility must print that (Tester could do it instead), we could start using it as a regular test function without further considerations.

--- src/test_utils.hpp
+++ src/test_utils.hpp
@@ -20,31 +20,41 @@ Type::Value* eval_expression(const std::string& expr, Type::Environment& env) {
 	return Type::unboxed(eval(top_level_call.get(), env));
 }
 
-/**
- * T - C++ scalar type of the value to check against
- * U - Type::Value counterpart
- */
+// NOTE: Helper function for null and type checking.
+// Not supposed to be used, as it does not print `Success`.
+// Perhaps the printing of such string can be deprecated in the future.
+int of_type(Type::Value* v, value_type v_type) {
+
+	if(!v) {
+		std::cerr << "ERROR: Null result\n";
+		return 1;
+	}
+
+	if(v->type() != v_type) {
+		// TODO: better error message
+		std::cerr << "ERROR: Type discrepancy\n";
+		return 2;
+	}
+
+	return 0;
+}
+
+// T - C++ scalar type of the value to check against
+// U - Type::Value counterpart
 template <typename T, typename U>
 int scalar_equals(
     const std::string& expr, const value_type v_type, const T& value, Type::Environment& env
 ) {
 	auto* result = eval_expression(expr, env);
 
-	if(!result) {
-		std::cerr << "ERROR: Null result\n";
-		return 1;
-	}
-
-	if(result->type() != v_type) {
-		// TODO: better error message
-		std::cerr << "ERROR: Type discrepancy\n";
-		return 2;
-	}
+	int fail = of_type(result, v_type);
+	if (fail)
+		return fail;
 
 	auto* as_u = static_cast<U*>(result);
 	if(as_u->m_value != value){
 		std::cerr
-			<< "ERROR\n"
+			<< "ERROR:\n"
 			<< "Expected: " << value << std::endl
 			<< "Got: " << as_u->m_value << std::endl;
 		return 3;
@@ -82,21 +92,33 @@ int is_false(std::string const& expr, Type::Environment& env) {
 int is_null(std::string const& expr, Type::Environment& env) {
 	auto* result = eval_expression(expr, env);
 
-	if(!result) {
-		std::cerr << "ERROR: Null result\n";
-		return 1;
-	}
-
-	if(result->type() != value_type::Null) {
-		// TODO: better error message
-		std::cerr << "ERROR: Type discrepancy\n";
-		return 2;
-	}
+	int fail = of_type(result, value_type::Null);
+	if (fail)
+		return fail;
 
 	std::cout << "Success\n";
 	return 0;
 }
 
-// TODO: array assertions
+int array_of_size(std::string const& expr, int size, Type::Environment& env) {
+	assert(size >= 0);
+	auto* result = Assert::eval_expression(expr, env);
+
+	int fail = of_type(result, value_type::Array);
+	if (fail)
+		return fail;
+
+	auto* array = static_cast<Type::Array*>(result);
+	if (array->m_value.size() != size) {
+		std::cerr
+			<< "ERROR:\n"
+			<< "Expected: array of size" << size << std::endl
+			<< "Got: array of size " << array->m_value.size() << std::endl;
+		return 3;
+	}
+
+	std::cout << "Success\n";
+	return 0;
+}
 
 } // Assert
