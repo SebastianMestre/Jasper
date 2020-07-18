d60bb7aaa0864049f7de8b8823262b2ed1eed212
Luciano Barletta <lushobarlett@gmail.com>
2020-07-18 14:47:52 -0300

    Tester: re-implement tester
    Now execute will display messages:
    '.' - Ok
    'E' - Error during testing
    'F' - Test assertion failed
    'R' - Risky test (source code with no functions)
    
    Followed by a list of errors in order of appearance

--- src/tester.cpp
+++ src/tester.cpp
@@ -1,33 +1,59 @@
 #include "tester.hpp"
 
+#include <iostream>
 #include <string>
 #include <vector>
 
-#include "eval.hpp"
-#include "execute.hpp"
-#include "garbage_collector.hpp"
+#include "test_type.hpp"
 
 namespace Test {
 
-Tester::Tester(std::string s) : m_source(std::move(s)) {};
-Tester::Tester(std::string s, std::vector<TestFunction> tfs)
-    : m_source(std::move(s)), m_testers(std::move(tfs)) {};
+Tester::Tester(TestSet ts) : m_test_sets({ std::move(ts) }) {};
+Tester::Tester(std::vector<TestSet> tss) : m_test_sets(std::move(tss)) {};
 
-void Tester::add_test(TestFunction tf) {
-	m_testers.push_back(tf);
+void Tester::add_test(TestSet ts) {
+	m_test_sets.push_back(ts);
 }
 
-void Tester::add_tests(const std::vector<TestFunction>& tfs) {
-	for (auto* tf : tfs)
-		m_testers.push_back(tf);
+void Tester::add_tests(const std::vector<TestSet>& tss) {
+	for (auto ts : tss)
+		m_test_sets.push_back(ts);
 }
 
-int Tester::execute(bool print_parse) {
-	for (auto* tester : m_testers) {
-		int exit_code = ::execute(m_source, print_parse, tester);
-		if(exit_code != 0) return exit_code;
+void Tester::execute(bool dump_ast) {
+	// TODO: dump ast to file to prevent
+	// unreadable test output
+	std::vector<test_type> errors;
+
+	for (auto ts : m_test_sets) {
+		test_type ts_answer = ts.execute(dump_ast);
+		switch(ts_answer){
+		case test_type::Ok:
+			std::cout << '.';
+			break;
+		case test_type::ParseError:
+		case test_type::TopLevelTypeError:
+			errors.push_back(ts_answer);
+			std::cout << 'E';
+			break;
+		case test_type::NullResult:
+		case test_type::TypeError:
+		case test_type::ValueError:
+			errors.push_back(ts_answer);
+			std::cout << 'F';
+			break;
+		case test_type::Empty:
+			errors.push_back(ts_answer);
+			std::cout << 'R';
+			break;
+		default:
+			std::cout << " -Unknown test code- ";
+		}
 	}
-	return 0;
+	std::cout << std::endl;
+
+	for(auto tt : errors)
+		std::cout << test_type_string[static_cast<int>(tt)] << std::endl;
 }
 
 }
