8fccdd6a52e23a29017ab5929d7be2386cadaebe
Luciano Barletta <lushobarlett@gmail.com>
2020-07-20 12:10:37 -0300

    Execute: fix memory leak

--- src/execute.cpp
+++ src/execute.cpp
@@ -10,13 +10,13 @@
 #include "token_array.hpp"
 #include "typed_ast.hpp"
 
-test_type execute(std::string const& source, bool dump_ast, Runner* runner) {
+exit_status_type execute(std::string const& source, bool dump_ast, Runner* runner) {
 
 	TokenArray ta;
 	auto parse_result = parse_program(source, ta);
 	if (not parse_result.ok()) {
 		parse_result.m_error.print();
-		return test_type::ParseError;
+		return exit_status_type::ParseError;
 	}
 
 	if (dump_ast)
@@ -28,7 +28,7 @@ test_type execute(std::string const& source, bool dump_ast, Runner* runner) {
 
 	auto top_level_ast = std::move(parse_result.m_result);
 	if (top_level_ast->type() != ast_type::DeclarationList)
-		return test_type::TopLevelTypeError;
+		return exit_status_type::TopLevelTypeError;
 
 	declare_native_functions(env);
 
@@ -37,18 +37,16 @@ test_type execute(std::string const& source, bool dump_ast, Runner* runner) {
 	gather_captures(top_level.get());
 	eval(top_level.get(), env);
 
-	test_type runner_exit_code = runner(env);
-
-	if(test_type::Ok != runner_exit_code)
-		return runner_exit_code;
+	exit_status_type runner_exit_code = runner(env);
 
 	gc.run();
 
-	return test_type::Ok;
+	return runner_exit_code;
 }
 
 // NOTE: We currently implement funcion evaluation in eval(AST::CallExpression)
 // this means we need to create a call expression node to run the program.
+// Having the TokenArray die here is not good, as it takes ownership of the tokens
 // TODO: We need to clean this up
 Type::Value* eval_expression(const std::string& expr, Type::Environment& env) {
 	TokenArray ta;
--- src/execute.hpp
+++ src/execute.hpp
@@ -2,13 +2,13 @@
 
 #include <string>
 #include "environment_fwd.hpp"
-#include "test_type.hpp"
+#include "exit_status_type.hpp"
 #include "value.hpp"
 
-using Runner = auto (Type::Environment&) -> test_type;
+using Runner = auto (Type::Environment&) -> exit_status_type;
 
 // returns an exit status
-test_type execute(std::string const& source, bool dump_ast, Runner* runner);
+exit_status_type execute(std::string const& source, bool dump_ast, Runner* runner);
 
 // creates an expression node and returns the unboxed value from it
 Type::Value* eval_expression(const std::string& expr, Type::Environment& env);
