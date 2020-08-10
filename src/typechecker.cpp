18bdbbbce18a5b5ef6a9ef6c3bcf048f4d6e440e
SebastianMestre <mestresebastianunr@gmail.com>
2020-08-09 23:47:57 -0300

    Add some more builtin types to the type checker

--- src/typechecker.cpp
+++ src/typechecker.cpp
@@ -11,6 +11,8 @@ TypeChecker::TypeChecker() {
 	m_core.type_function_data.push_back({  0 }); // 1 | int
 	m_core.type_function_data.push_back({  0 }); // 2 | float
 	m_core.type_function_data.push_back({  0 }); // 3 | string
+	m_core.type_function_data.push_back({  1 }); // 4 | array
+	m_core.type_function_data.push_back({  1 }); // 5 | dictionary
 
 	m_core.term_data.push_back({ 1, {}}); // 0 | int(<>)
 	m_core.term_data.push_back({ 2, {}}); // 1 | float(<>)
@@ -20,8 +22,6 @@ TypeChecker::TypeChecker() {
 	m_core.mono_data.push_back({ mono_type::Term, 1 }); // 1 | float(<>)
 	m_core.mono_data.push_back({ mono_type::Term, 2 }); // 2 | string(<>)
 }
-
-TypeFunctionId tf_function() { return 0; }
 
 MonoId TypeChecker::mono_int() { return 0; }
 MonoId TypeChecker::mono_float() { return 1; }
@@ -34,10 +34,10 @@ MonoId TypeChecker::rule_var(PolyId poly) {
 // Hindley-Milner [App], modified for multiple argument functions.
 MonoId TypeChecker::rule_app(std::vector<MonoId> args_types, MonoId func_type) {
 	MonoId return_type = m_core.new_var();
-
 	args_types.push_back(return_type);
+
 	MonoId deduced_func_type
-	    = m_core.new_term(tf_function(), std::move(args_types));
+	    = m_core.new_term(BuiltinType::Function, std::move(args_types));
 
 	m_core.unify(func_type, deduced_func_type);
 
