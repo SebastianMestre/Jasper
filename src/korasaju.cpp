da875a054a7b888f348b6ceb1364d65acfcf47c6
SebastianMestre <mestresebastianunr@gmail.com>
2020-08-14 15:14:40 -0300

    formatting

--- src/korasaju.cpp
+++ src/korasaju.cpp
@@ -17,7 +17,7 @@ private:
 
 	std::vector<int> vertex_order;
 
-	int new_component () {
+	int new_component() {
 		int c = component_graph.size();
 		component_graph.push_back({});
 		component_vertices.push_back({});
@@ -25,18 +25,18 @@ private:
 	}
 
 	void visit(int u) {
-		if(visited[u])
+		if (visited[u])
 			return;
 
 		visited[u] = true;
-		for (int v : graph[u]){
+		for (int v : graph[u]) {
 			visit(v);
 			vertex_order.push_back(u);
 		}
 	}
 
 	void assign(int u, int root) {
-		if(component_of_vertex[u] != -1)
+		if (component_of_vertex[u] != -1)
 			return;
 
 		component_of_vertex[u] = root;
@@ -63,10 +63,10 @@ public:
 		graph[j].push_back(i);
 	}
 
-	void solve () {
+	void solve() {
 		const int n = graph.size();
 
-		for(int u = 0; u != n; ++u)
+		for (int u = 0; u != n; ++u)
 			visit(u);
 
 		assert(vertex_order.size() == n);
@@ -74,16 +74,16 @@ public:
 		std::reverse(vertex_order.begin(), vertex_order.end());
 
 		component_of_vertex.assign(n, -1);
-		for(int i = 0; i != n; ++i){
+		for (int i = 0; i != n; ++i) {
 			int u = vertex_order[i];
 			assign(u, new_component());
 		}
 
-		for(int u = 0; u != n; ++u){
+		for (int u = 0; u != n; ++u) {
 			int source_component = component_of_vertex[u];
-			for(int v : graph[u]){
+			for (int v : graph[u]) {
 				int destination_component = component_of_vertex[v];
-				if(source_component == destination_component)
+				if (source_component == destination_component)
 					continue;
 
 				component_graph[source_component].insert(destination_component);
