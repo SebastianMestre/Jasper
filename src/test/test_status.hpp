ebe2c8617c9c7dacdbbe05d64dfc7f6a201affcb
Luciano Barletta <lushobarlett@gmail.com>
2020-08-30 00:01:34 -0300

    Tests: add test-specific codes and a POD struct

---
+++ src/test/test_status.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+constexpr const char* test_status_string[] = {
+	"Ok",
+	"Error",
+	"Fail",
+	"Empty",
+};
+
+enum class test_status {
+	Ok = 0,
+	Error,
+	Fail,
+	Empty,
+};
+
+struct TestReport {
+	test_status m_code;
+	std::string m_msg;
+};
