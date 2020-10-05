eca952841f73f2c5878f08fcf47aa38313e24943
Luciano Barletta <lushobarlett@gmail.com>
2020-10-05 17:15:36 -0300

    separate code into recursive and regular templated structs
    the recursive struct matches the type, and then leaves the implementation of each function to the regular struct

--- src/node_allocator.hpp
+++ src/node_allocator.hpp
@@ -5,23 +5,15 @@
 #include <vector>
 #include <type_traits>
 
-template <typename ...Ts>
-struct NodeAllocator;
-
-// base case
 template <typename T>
-struct NodeAllocator<T> {
+struct SingleAllocator {
 
 	std::vector<T> m_nodes;
 
-	template <typename U>
-	U* get(int index) {
-		// has to be here, otherwise error
-		static_assert(std::is_same<T, U>::value, "requested non-existent node");
+	T* get(int index) {
 		return &m_nodes[index];
 	}
 
-	template <typename U>
 	int make() {
 		int index = m_nodes.size();
 		m_nodes.push_back({});
@@ -29,37 +21,40 @@ struct NodeAllocator<T> {
 	}
 };
 
+template <typename ...Ts>
+struct NodeAllocator;
+
+template <>
+struct NodeAllocator<> {
+
+	template <typename U>
+	U* get(int index) { assert(0 and "No matching type"); }
+
+	template <typename U>
+	int make() { assert(0 and "No matching type"); }
+};
+
 template <typename T, typename ...Ts>
 struct NodeAllocator<T, Ts...> {
 
-	std::vector<T> m_nodes;
+	SingleAllocator<T> m_allocator;
 	NodeAllocator<Ts...> m_allocators;
 
 	// it's here
 	template <typename U>
 	typename std::enable_if<std::is_same<T, U>::value, U*>::type
-	get(int index) {
-		return &m_nodes[index];
-	}
+	get(int index) { return m_allocator.get(index); }
 
 	template <typename U>
 	typename std::enable_if<std::is_same<T, U>::value, int>::type
-	make() {
-		int index = m_nodes.size();
-		m_nodes.push_back({});
-		return index;
-	}
+	make() { return m_allocator.make(); }
 
 	// it's not here
 	template <typename U>
 	typename std::enable_if<!std::is_same<T, U>::value, U*>::type
-	get(int index) {
-		return m_allocators.template get<U>(index);
-	}
+	get(int index) { return m_allocators.template get<U>(index); }
 
 	template <typename U>
 	typename std::enable_if<!std::is_same<T, U>::value, int>::type
-	make() {
-		return m_allocators.template make<U>();
-	}
+	make() { return m_allocators.template make<U>(); }
 };
