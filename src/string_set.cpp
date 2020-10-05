fba08072490de03017b402accd0b21aa419fb46b
SebastianMestre <mestresebastianunr@gmail.com>
2020-10-04 21:34:12 -0300

    remove duplication, add bugs

--- src/string_set.cpp
+++ src/string_set.cpp
@@ -5,6 +5,8 @@
 #include <cassert>
 #include <cstring>
 
+#include <iostream>
+
 // compute a 64-bit hash using a simple horner hashing scheme
 static uint64_t compute_hash(unsigned char const* data, size_t length) {
 	constexpr uint64_t base = 263;
@@ -37,61 +39,54 @@ StringSet::StringSet(){
 	m_data.resize(8);
 }
 
-void StringSet::insert(std::string const& s) {
+std::pair<std::string const*, bool> StringSet::insert(std::string&& s) {
 	uint64_t hash_bits = compute_effective_hash(
 	    reinterpret_cast<unsigned char const*>(s.data()), s.size());
-	auto pos = insertion_spot(s.data(), s.size(), hash_bits);
+	auto pos = scan(s.data(), s.size(), hash_bits);
 
-	if (pos.second)
-		return;
+	if (pos.found)
+		return {m_data[pos.stop_index].second.get(), false};
 
 	if (m_size * 2 >= m_data.size())
 		rehash(m_data.size() * 2);
 
 	m_size += 1;
-	put(pos.first, std::string {s}, hash_bits);
+	put(pos.free_index, std::move(s), hash_bits);
+
+	return {m_data[pos.free_index].second.get(), true};
 }
 
-void StringSet::insert(std::string&& s) {
-	uint64_t hash_bits = compute_effective_hash(
-	    reinterpret_cast<unsigned char const*>(s.data()), s.size());
-	auto pos = insertion_spot(s.data(), s.size(), hash_bits);
-
-	if (pos.second)
-		return;
-
-	if (m_size * 2 >= m_data.size())
-		rehash(m_data.size() * 2);
-
-	m_size += 1;
-	put(pos.first, std::move(s), hash_bits);
-}
-
-void StringSet::insert(char const* data, size_t length) {
+std::pair<std::string const*, bool> StringSet::insert(char const* data, size_t length) {
 	uint64_t hash_bits = compute_effective_hash(
 	    reinterpret_cast<unsigned char const*>(data), length);
-	auto pos = insertion_spot(data, length, hash_bits);
+	auto pos = scan(data, length, hash_bits);
 
-	if (pos.second)
-		return;
+	if (pos.found)
+		return {m_data[pos.stop_index].second.get(), false};
 
 	if (m_size * 2 >= m_data.size())
 		rehash(m_data.size() * 2);
 
 	m_size += 1;
-	put(pos.first, std::string {data, length}, hash_bits);
+	put(pos.free_index, std::string {data, length}, hash_bits);
+
+	return {m_data[pos.free_index].second.get(), true};
 }
 
-void StringSet::insert(char const* data) {
-	insert(data, strlen(data));
+std::pair<std::string const*, bool> StringSet::insert(std::string const& s) {
+	return insert(s.data(), s.size());
+}
+
+std::pair<std::string const*, bool> StringSet::insert(char const* data) {
+	return insert(data, strlen(data));
 }
 
 bool StringSet::includes(char const* data, size_t length) const {
 	uint64_t hash_bits = compute_effective_hash(
 	    reinterpret_cast<unsigned char const*>(data), length);
-	auto pos = find_spot(data, length, hash_bits);
+	auto pos = scan(data, length, hash_bits);
 
-	return pos.second;
+	return pos.found;
 }
 
 bool StringSet::includes(char const* data) const {
@@ -115,47 +110,35 @@ void StringSet::rehash(size_t new_size) {
 	for (auto& slot : old_data){
 		if(slot.first.status == HashField::Occupied){
 			uint64_t hash_bits = slot.first.hash_bits;
-			auto pos = insertion_spot(
-			    slot.second->data(),
-			    slot.second->size(),
-			    hash_bits);
-			put(pos.first, std::move(*slot.second), hash_bits);
+			auto pos = scan(slot.second->data(), slot.second->size(), hash_bits);
+			put(pos.free_index, std::move(*slot.second), hash_bits);
 		}
 	}
 }
 
-std::pair<int, bool> StringSet::insertion_spot(char const* data, size_t length, uint64_t hash_bits) const {
-	assert(m_data.size() > m_size);
 
-	int position = hash_bits % m_data.size();
-	bool found = false;
-	while (m_data[position].first.status == HashField::Occupied) {
+StringSet::ScanData StringSet::scan(char const* data, size_t length, uint64_t hash_bits) const {
 
-		if (length == m_data[position].second->size() &&
-		    memcmp(data, m_data[position].second->data(), length) == 0) {
-			found = true;
-			break;
-		}
-
-		position += 1;
-		if (position == static_cast<int>(m_data.size()))
-			position = 0;
+	if (m_data.size() <= m_size){
+		*(volatile int*)0 = 10;
 	}
 
-	return {position, found};
-}
-
-std::pair<int, bool> StringSet::find_spot(char const* data, size_t length, uint64_t hash_bits) const {
 	assert(m_data.size() > m_size);
 
 	int position = hash_bits % m_data.size();
+	int free_position = -1;
 	bool found = false;
 	while (m_data[position].first.status != HashField::Empty) {
 
-		if (length == m_data[position].second->size() &&
-		    memcmp(data, m_data[position].second->data(), length) == 0) {
-			found = true;
-			break;
+		if (m_data[position].first.status == HashField::Tombstone) {
+			if (free_position == -1)
+				free_position = position;
+		} else if (m_data[position].first.status == HashField::Occupied) {
+			if (length == m_data[position].second->size() &&
+			    memcmp(data, m_data[position].second->data(), length) == 0) {
+				found = true;
+				break;
+			}
 		}
 
 		position += 1;
@@ -163,10 +146,16 @@ std::pair<int, bool> StringSet::find_spot(char const* data, size_t length, uint6
 			position = 0;
 	}
 
-	return {position, found};
+	if (free_position == -1 && m_data[position].first.status == HashField::Empty) {
+		free_position = position;
+	}
+
+	return {free_position, position, found};
 }
 
 void StringSet::put(int position, std::string&& str, uint64_t hash_bits){
+	std::cerr << "PUTTING AT " << position << " -- SIZE IS " << m_data.size() << "\n";
+
 	assert(m_data[position].first.status != HashField::Occupied);
 
 	m_data[position].first.status = HashField::Occupied;
--- src/string_set.hpp
+++ src/string_set.hpp
@@ -10,27 +10,33 @@ struct InternedString;
 
 // a flat linear hashing table
 struct StringSet {
+	struct ScanData {
+		int free_index;
+		int stop_index;
+		bool found;
+	};
+
 	struct HashField {
 		// possible status values:
 		static constexpr uint64_t Empty = 0;
 		static constexpr uint64_t Occupied = 1;
 		static constexpr uint64_t Tombstone = 2;
 
-		uint64_t status : 2;
-		uint64_t hash_bits : 62;
+		uint64_t status : 2 {0};
+		uint64_t hash_bits : 62 {0};
 	};
 
 	std::vector<std::pair<HashField, std::unique_ptr<std::string>>> m_data;
-	size_t m_size;
+	size_t m_size {0};
 
 	StringSet();
 
-	void insert(std::string const&);
-	void insert(std::string&&);
+	std::pair<std::string const*, bool> insert(std::string const&);
+	std::pair<std::string const*, bool> insert(std::string&&);
 	// void insert(InternedString const&); // TODO
 	// void insert(string_view const&); // TODO
-	void insert(char const*, size_t);
-	void insert(char const*);
+	std::pair<std::string const*, bool> insert(char const*, size_t);
+	std::pair<std::string const*, bool> insert(char const*);
 
 	bool includes(std::string const&) const;
 	// bool includes(InternedString const&) const; // TODO
@@ -39,8 +45,7 @@ struct StringSet {
 	bool includes(char const*) const;
 
   private:
-	std::pair<int, bool> insertion_spot(char const*, size_t, uint64_t) const;
-	std::pair<int, bool> find_spot(char const*, size_t, uint64_t) const;
+	ScanData scan(char const*, size_t, uint64_t) const;
 	void put(int, std::string&&, uint64_t);
 	void rehash(size_t);
 };
