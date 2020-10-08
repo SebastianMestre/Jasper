b64001e81050ed9b8f4c18148be4871b4cf73671
Luciano Barletta <lushobarlett@gmail.com>
2020-10-08 14:22:48 -0300

    add typed_ast_allocator structure

---
+++ src/typed_ast_allocator.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "typed_ast.hpp"
+#include "node_allocator.hpp"
+
+namespace TypedAST {
+
+// TODO: could we use the TYPED_AST_TAGS macro here?
+using Allocator = NodeAllocator<
+    NumberLiteral,
+    IntegerLiteral,
+    StringLiteral,
+    BooleanLiteral,
+    NullLiteral,
+    ObjectLiteral,
+    ArrayLiteral,
+    DictionaryLiteral,
+    FunctionLiteral,
+    Identifier,
+    CallExpression,
+    IndexExpression,
+    RecordAccessExpression,
+    TernaryExpression,
+    Block,
+    ReturnStatement,
+    IfElseStatement,
+    ForStatement,
+    WhileStatement,
+    DeclarationList,
+    Declaration,
+    StructExpression,
+    TypeTerm,
+    TypeFunctionHandle,
+    MonoTypeHandle>;
+
+} // namespace TypedAST
