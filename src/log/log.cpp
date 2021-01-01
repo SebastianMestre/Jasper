739a4acf513279af135ce80137abd8a5b68882f5
SebastianMestre <mestresebastianunr@gmail.com>
2021-01-01 11:50:44 -0300

    Log: add some other useful functions

--- src/log/log.cpp
+++ src/log/log.cpp
@@ -8,4 +8,13 @@ void warning(char const* str) {
 	std::clog << "[ Warning ] " << str << "\n";
 }
 
+void error(char const* str) {
+	std::cerr << "[ Error ] " << str << "\n";
+}
+
+void fatal(char const* str) {
+	std::cerr << "[ Fatal Error ] " << str << "\n";
+	exit(1);
+}
+
 } // namespace Log
--- src/log/log.hpp
+++ src/log/log.hpp
@@ -3,4 +3,8 @@ namespace Log {
 
 void warning(char const*);
 
+void error(char const*);
+
+void fatal(char const*);
+
 }
