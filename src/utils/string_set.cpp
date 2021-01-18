732554f54d6d5ab6137437d0a05581310d049c48
SebastianMestre <mestresebastianunr@gmail.com>
2021-01-18 20:20:42 -0300

    change StringSet data representation

--- src/utils/string_set.cpp
+++ src/utils/string_set.cpp
@@ -24,41 +24,44 @@ static uint64_t compute_effective_hash(unsigned char const* data, size_t length)
 // ==== ==== ==== ====
 
 StringSet::StringSet() {
-	m_data.resize(8);
-	memset(m_data.data(), 0, sizeof(m_data[0]) * m_data.size());
+	constexpr int initial_size = 8;
+	m_slot.resize(initial_size);
+	memset(m_slot.data(), 0, sizeof(m_slot[0]) * initial_size);
+	m_value.resize(initial_size);
+	memset(m_value.data(), 0, sizeof(m_value[0]) * initial_size);
 }
 
 std::pair<std::string const*, bool> StringSet::insert(std::string&& s) {
-	if (m_size * 2 >= m_data.size())
-		rehash(m_data.size() * 2);
+	if (m_size * 2 >= m_slot.size())
+		rehash(m_slot.size() * 2);
 
 	uint64_t hash_bits = compute_effective_hash(
 	    reinterpret_cast<unsigned char const*>(s.data()), s.size());
 	auto pos = scan(s.data(), s.size(), hash_bits);
 
 	if (pos.found)
-		return {m_data[pos.stop_index].value, false};
+		return {m_value[pos.stop_index], false};
 
 	m_size += 1;
 	put(pos.free_index, std::move(s), hash_bits);
 
-	return {m_data[pos.free_index].value, true};
+	return {m_value[pos.free_index], true};
 }
 
 std::pair<std::string const*, bool> StringSet::insert(char const* data, size_t length) {
-	if (m_size * 2 >= m_data.size())
-		rehash(m_data.size() * 2);
+	if (m_size * 2 >= m_slot.size())
+		rehash(m_slot.size() * 2);
 
 	uint64_t hash_bits = compute_effective_hash(
 	    reinterpret_cast<unsigned char const*>(data), length);
 	auto pos = scan(data, length, hash_bits);
 	if (pos.found)
-		return {m_data[pos.stop_index].value, false};
+		return {m_value[pos.stop_index], false};
 
 	m_size += 1;
 	put(pos.free_index, std::string {data, length}, hash_bits);
 
-	return {m_data[pos.free_index].value, true};
+	return {m_value[pos.free_index], true};
 }
 
 std::pair<std::string const*, bool> StringSet::insert(std::string const& s) {
@@ -87,47 +90,56 @@ bool StringSet::includes(std::string const& str) const {
 
 // ==== ==== ==== ====
 
-void StringSet::rehash(size_t new_size) {
-	if (new_size < m_data.size())
+void StringSet::rehash(size_t const new_size) {
+	if (new_size < m_slot.size())
 		return;
 
-	std::vector<HashField> old_data = std::move(m_data);
+	std::vector<HashField> old_slot = std::move(m_slot);
+	m_slot.clear();
+	m_slot.resize(new_size);
+	memset(m_slot.data(), 0, sizeof(m_slot[0]) * new_size);
 
-	m_data.clear();
-	m_data.resize(new_size);
-	memset(m_data.data(), 0, sizeof(m_data[0]) * m_data.size());
+	std::vector<std::string*> old_value = std::move(m_value);
+	m_value.clear();
+	m_value.resize(new_size);
+	memset(m_value.data(), 0, sizeof(m_value[0]) * new_size);
 
 	m_size = 0;
-	for (auto& slot : old_data) {
+	const int n = old_slot.size();
+	for (int i = 0; i < n; ++i) {
+		auto& slot = old_slot[i];
+
 		if (slot.status != HashField::Occupied)
 			continue;
-		auto pos = scan(slot.value->data(), slot.value->size(), slot.hash_bits);
+
+		auto value = old_value[i];
+		auto pos = scan(value->data(), value->size(), slot.hash_bits);
 		assert(!pos.found);
-		m_data[pos.free_index].value = slot.value;
-		m_data[pos.free_index].status = HashField::Occupied;
-		m_data[pos.free_index].hash_bits = slot.hash_bits;
+		m_value[pos.free_index] = value;
+		m_slot[pos.free_index].status = HashField::Occupied;
+		m_slot[pos.free_index].hash_bits = slot.hash_bits;
 		m_size += 1;
 	}
 }
 
 StringSet::ScanData StringSet::scan(
     char const* data, size_t length, uint64_t hash_bits) const {
-	assert(m_data.size() > m_size);
+	assert(m_slot.size() > m_size);
 
-	const int capacity_mask = m_data.size() - 1;
+	const int capacity_mask = m_slot.size() - 1;
 	int position = hash_bits & capacity_mask;
 	int free_position = -1;
 
 	while (true) {
-		if (m_data[position].status == HashField::Occupied) {
-			if (m_data[position].hash_bits == hash_bits &&
-			    length == m_data[position].value->size() &&
-			    memcmp(data, m_data[position].value->data(), length) == 0)
+		if (m_slot[position].status == HashField::Occupied) {
+			if (m_slot[position].hash_bits == hash_bits &&
+			    length == m_value[position]->size() &&
+			    memcmp(data, m_value[position]->data(), length) == 0)
 				return {free_position, position, true};
 		} else {
 			if (free_position == -1)
 				free_position = position;
-			if (m_data[position].status == HashField::Empty)
+			if (m_slot[position].status == HashField::Empty)
 				return {free_position, position, false};
 		}
 
@@ -141,7 +153,7 @@ void StringSet::put(int position, std::string&& str, uint64_t hash_bits) {
 	assert((hash_bits >> 62) == 0);
 
 	m_storage.push_back(std::move(str));
-	m_data[position].value = &m_storage.back();
-	m_data[position].status = HashField::Occupied;
-	m_data[position].hash_bits = hash_bits;
+	m_value[position] = &m_storage.back();
+	m_slot[position].status = HashField::Occupied;
+	m_slot[position].hash_bits = hash_bits;
 }
