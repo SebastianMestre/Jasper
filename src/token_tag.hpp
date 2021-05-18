255074c64bab232d0c3b62c0944895ae8b619cc2
SebastianMestre <mestresebastianunr@gmail.com>
2021-05-17 22:08:54 -0300

    use a composite xmacro in token_tags.hpp

--- src/token_tag.hpp
+++ src/token_tag.hpp
@@ -1,171 +1,81 @@
 #pragma once
 
+#define TOKEN_TAGS                                                             \
+	X(ADD, "+")                                                                \
+	X(SUB, "-")                                                                \
+	X(DIV, "/")                                                                \
+	X(MUL, "*")                                                                \
+	X(AND, "&")                                                                \
+	X(IOR, "|")                                                                \
+	X(XOR, "^")                                                                \
+	X(LT, "<")                                                                 \
+	X(GT, ">")                                                                 \
+	X(LTE, "<=")                                                               \
+	X(GTE, ">=")                                                               \
+	X(COMPL, "COMPL")                                                          \
+	X(LOGIC_COMPL, "LOGIC_COMPL")                                              \
+	X(LOGIC_AND, "LOGIC_AND")                                                  \
+	X(LOGIC_IOR, "LOGIC_IOR")                                                  \
+	X(EQUAL, "==")                                                             \
+	X(NOT_EQUAL, "!=")                                                         \
+	X(ASSIGN, "=")                                                             \
+	X(DECLARE_ASSIGN, ":=")                                                    \
+	X(DECLARE, ":")                                                            \
+	X(INCREMENT, "INCREMENT")                                                  \
+	X(DECREMENT, "DECREMENT")                                                  \
+	X(ADD_TO, "ADD_TO")                                                        \
+	X(SUB_TO, "SUB_TO")                                                        \
+	X(MUL_TO, "MUL_TO")                                                        \
+	X(DIV_TO, "DIV_TO")                                                        \
+	X(AND_TO, "AND_TO")                                                        \
+	X(IOR_TO, "IOR_TO")                                                        \
+	X(XOR_TO, "XOR_TO")                                                        \
+	X(AT, "AT")                                                                \
+	X(DOT, "DOT")                                                              \
+	X(COMMA, "COMMA")                                                          \
+	X(PIZZA, "PIZZA")                                                          \
+	X(ARROW, "ARROW")                                                          \
+	X(BRACE_OPEN, "BRACE_OPEN")                                                \
+	X(BRACKET_OPEN, "BRACKET_OPEN")                                            \
+	X(PAREN_OPEN, "PAREN_OPEN")                                                \
+	X(POLY_OPEN, "POLY_OPEN")                                                  \
+	X(BRACE_CLOSE, "BRACE_CLOSE")                                              \
+	X(BRACKET_CLOSE, "BRACKET_CLOSE")                                          \
+	X(PAREN_CLOSE, "PAREN_CLOSE")                                              \
+	X(POLY_CLOSE, "POLY_CLOSE")                                                \
+	X(SEMICOLON, "SEMICOLON")                                                  \
+	X(COLON, "COLON")                                                          \
+	X(NUMBER, "NUMBER")                                                        \
+	X(INTEGER, "INTEGER")                                                      \
+	X(STRING, "STRING")                                                        \
+	X(KEYWORD_FALSE, "KEYWORD_FALSE")                                          \
+	X(KEYWORD_TRUE, "KEYWORD_TRUE")                                            \
+	X(KEYWORD_NULL, "KEYWORD_NULL")                                            \
+	X(IDENTIFIER, "IDENTIFIER")                                                \
+	X(KEYWORD, "KEYWORD")                                                      \
+	X(KEYWORD_FN, "KEYWORD_FN")                                                \
+	X(KEYWORD_ARRAY, "KEYWORD_ARRAY")                                          \
+	X(KEYWORD_RETURN, "KEYWORD_RETURN")                                        \
+	X(KEYWORD_IF, "KEYWORD_IF")                                                \
+	X(KEYWORD_THEN, "KEYWORD_THEN")                                            \
+	X(KEYWORD_ELSE, "KEYWORD_ELSE")                                            \
+	X(KEYWORD_FOR, "KEYWORD_FOR")                                              \
+	X(KEYWORD_WHILE, "KEYWORD_WHILE")                                          \
+	X(KEYWORD_MATCH, "KEYWORD_MATCH")                                          \
+	X(KEYWORD_SEQ, "KEYWORD_SEQ")                                              \
+	X(KEYWORD_UNION, "KEYWORD_UNION")                                          \
+	X(KEYWORD_TUPLE, "KEYWORD_TUPLE")                                          \
+	X(KEYWORD_STRUCT, "KEYWORD_STRUCT")                                        \
+	X(END, "(end of file)")
+
 /* printable representation */
-constexpr const char* token_string[] = {
-	"+",
-	"-",
-	"/",
-	"*",
-	"&",
-	"|",
-	"^",
-
-	"<",
-	">",
-	"<=",
-	">=",
-
-	"COMPL",
-
-	"LOGIC_COMPL",
-	"LOGIC_AND",
-	"LOGIC_IOR",
-
-	"==",
-	"!=",
-
-	"=",
-	":=",
-	":",
-
-	"INCREMENT",
-	"DECREMENT",
-
-	"ADD_TO",
-	"SUB_TO",
-	"MUL_TO",
-	"DIV_TO",
-	"AND_TO",
-	"IOR_TO",
-	"XOR_TO",
-
-	"AT",
-	"DOT",
-	"COMMA",
-	"PIZZA",
-	"ARROW",
-
-	"BRACE_OPEN",
-	"BRACKET_OPEN",
-	"PAREN_OPEN",
-	"POLY_OPEN",
-
-	"BRACE_CLOSE",
-	"BRACKET_CLOSE",
-	"PAREN_CLOSE",
-	"POLY_CLOSE",
-
-	"SEMICOLON",
-	"COLON",
-
-	"NUMBER",
-	"INTEGER",
-	"STRING",
-	"KEYWORD_FALSE",
-	"KEYWORD_TRUE",
-	"KEYWORD_NULL",
-	"IDENTIFIER",
-	"KEYWORD",
-
-	"KEYWORD_FN",
-	"KEYWORD_ARRAY",
-	"KEYWORD_RETURN",
-	"KEYWORD_IF",
-	"KEYWORD_THEN",
-	"KEYWORD_ELSE",
-	"KEYWORD_FOR",
-	"KEYWORD_WHILE",
-	"KEYWORD_MATCH",
-	"KEYWORD_SEQ",
-
-	"KEYWORD_UNION",
-	"KEYWORD_TUPLE",
-	"KEYWORD_STRUCT",
-
-	"END",
-};
+#define X(name, str) str,
+constexpr const char* token_string[] = {TOKEN_TAGS};
+#undef X
 
 /* internal representation */
-enum class TokenTag {
-	ADD,
-	SUB,
-	DIV,
-	MUL,
-	AND,
-	IOR,
-	XOR,
+#define X(name, str) name,
+enum class TokenTag { TOKEN_TAGS };
+#undef X
 
-	LT,
-	GT,
-	LTE,
-	GTE,
-
-	COMPL,
-
-	LOGIC_COMPL,
-	LOGIC_AND,
-	LOGIC_IOR,
-
-	EQUAL,
-	NOT_EQUAL,
-
-	ASSIGN,
-	DECLARE_ASSIGN,
-	DECLARE,
-
-	INCREMENT,
-	DECREMENT,
-
-	ADD_TO,
-	SUB_TO,
-	MUL_TO,
-	DIV_TO,
-	AND_TO,
-	IOR_TO,
-	XOR_TO,
-
-	AT,
-	DOT,
-	COMMA,
-	PIZZA,
-	ARROW,
-
-	BRACE_OPEN,
-	BRACKET_OPEN,
-	PAREN_OPEN,
-	POLY_OPEN,
-
-	BRACE_CLOSE,
-	BRACKET_CLOSE,
-	PAREN_CLOSE,
-	POLY_CLOSE,
-
-	SEMICOLON,
-	COLON,
-
-	NUMBER,
-	INTEGER,
-	STRING,
-	KEYWORD_FALSE,
-	KEYWORD_TRUE,
-	KEYWORD_NULL,
-	IDENTIFIER,
-	KEYWORD,
-
-	KEYWORD_FN,
-	KEYWORD_ARRAY,
-	KEYWORD_RETURN,
-	KEYWORD_IF,
-	KEYWORD_THEN,
-	KEYWORD_ELSE,
-	KEYWORD_FOR,
-	KEYWORD_WHILE,
-	KEYWORD_MATCH,
-	KEYWORD_SEQ,
-
-	KEYWORD_UNION,
-	KEYWORD_TUPLE,
-	KEYWORD_STRUCT,
-
-	END,
-};
+#undef TOKEN_TAGS
