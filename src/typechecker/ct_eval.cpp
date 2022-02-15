285f5efcec85e3a87d66cad17fc507ae7e2034e5
SebastianMestre <mestresebastianunr@gmail.com>
2022-02-15 10:33:37 -0300

    inline variable

--- src/typechecker/ct_eval.cpp
+++ src/typechecker/ct_eval.cpp
@@ -273,8 +273,8 @@ static MonoId compute_mono(
 
 			auto decl = identifier->m_declaration;
 			AST::AST* arg_handle = static_cast<AST::MonoTypeHandle*>(decl->m_value);
+			assert(decl->m_value->type() == ASTTag::MonoTypeHandle);
 
-			assert(arg_handle->type() == ASTTag::MonoTypeHandle);
 			MonoId mono = static_cast<AST::MonoTypeHandle*>(arg_handle)->m_value;
 			args.push_back(mono);
 		} else {
