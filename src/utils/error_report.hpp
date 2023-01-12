b2ef1a893d355685d20978a7783c31d89d63bc15
SebastianMestre <mestresebastianunr@gmail.com>
2023-01-12 16:12:04 -0300

    Utils: make ErrorReport hold multiple lines per node

--- src/utils/error_report.hpp
+++ src/utils/error_report.hpp
@@ -7,8 +7,23 @@
 #include "string_view.hpp"
 
 struct ErrorReport {
-	std::string m_text;
+	std::vector<std::string> m_lines;
 	std::vector<ErrorReport> m_sub_errors;
+
+	ErrorReport() = default;
+
+	ErrorReport(std::string text)
+	    : m_lines {{std::move(text)}} {}
+
+	ErrorReport(std::string text, std::vector<ErrorReport> sub_errors)
+	    : m_lines {{std::move(text)}}
+	    , m_sub_errors {std::move(sub_errors)} {}
+
+	static ErrorReport single(std::string);
+	static ErrorReport single(std::string, std::vector<ErrorReport>);
+
+	static ErrorReport multi(std::vector<std::string>);
+	static ErrorReport multi(std::vector<std::string>, std::vector<ErrorReport>);
 
 	bool ok() const;
 	void print(int d = 1) const;
