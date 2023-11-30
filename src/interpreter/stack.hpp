6c7892ef6a3cc8eb157d968b36e36f61ee6b6c88
SebastianMestre <mestresebastianunr@gmail.com>
2023-11-30 14:54:51 -0300

    Interpreter: make most of Stack's internals private

--- src/interpreter/stack.hpp
+++ src/interpreter/stack.hpp
@@ -8,11 +8,6 @@
 namespace Interpreter {
 
 struct Stack {
-	int m_frame_ptr {0};
-	int m_stack_ptr {0};
-	std::vector<Value> m_stack;
-	std::vector<int> m_fp_stack;
-	std::vector<int> m_sp_stack;
 
 	void start_frame(int size = 0);
 	void end_frame();
@@ -21,12 +16,18 @@ struct Stack {
 	void end_region();
 
 	void push(Value ref);
-
 	Value pop_unsafe();
 
 	Value& access(int offset);
 	Value& frame_at(int offset);
 	Span<Value> frame_range(int offset, int length);
+
+	std::vector<Value> m_stack;
+private:
+	int m_frame_ptr {0};
+	int m_stack_ptr {0};
+	std::vector<int> m_fp_stack;
+	std::vector<int> m_sp_stack;
 };
 
 } // namespace Interpreter
